ac3a049e430a10b3ae55fc81dfe93c63366d64b7
ZhuMon <richardlin0212@gmail.com>
2021-03-22 13:07:55 +0800

    Draw PR_curve
    * Add expanded answer
    * Try numpy to faster

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ GRADUAL_TRANSITION_FRAME = []
 LIKELIHOOD_REGION_SIZE = (8, 8)
 EDGE_DETECT_THRESHOLD = 125 # threshold to decide whether edge
 ANSWER = []
-TRUE_ANSWER = []
+EXPAND_ANSWER = []
 
 def check_fade_or_wipe(SD, index):
     global GRADUAL_TRANSITION_FRAME
@@ -33,6 +33,7 @@ def check_fade_or_wipe(SD, index):
                 print(
                     f"{GRADUAL_TRANSITION_FRAME[0]}~{GRADUAL_TRANSITION_FRAME[-1]}")
                 ANSWER.append(f"{GRADUAL_TRANSITION_FRAME[0]}~{GRADUAL_TRANSITION_FRAME[-1]}")
+                EXPAND_ANSWER += GRADUAL_TRANSITION_FRAME
  
             GRADUAL_TRANSITION_FRAME = []
         TWIN_COMPARISON_accum = TWIN_COMPARISON_Ts
@@ -52,7 +53,7 @@ def check_fade_or_wipe(SD, index):
         return False
 
 
-def pair_wise(first, second, _):
+def pair_wise(first, second, index):
     im1 = Image.open(first)
     im2 = Image.open(second)
 
@@ -60,8 +61,14 @@ def pair_wise(first, second, _):
     pix1 = im1.load()
     pix2 = im2.load()
 
+    pix1 = np.array(im1)
+    pix2 = np.array(im2)
     DP = 0
     # compare pixel in two frame at same position
+    print(pix1[0][1])
+    print(pix2[0][1])
+    print(abs(pix2 - pix1)[0][1])
+    sys.exit(1)
     for i in range(im1.size[0]):
         for j in range(im1.size[1]):
             # compare every intensity, and sum them
@@ -75,11 +82,9 @@ def pair_wise(first, second, _):
     # Normalize
     DP_n = DP/(im1.size[0]*im1.size[1]) * 100
     VALUE_FOR_PLOT.append(DP_n)    
-    if DP_n > PAIR_WISE_T:
-        return True
-    else:
+    if DRY:
         return False
-
+    return check_fade_or_wipe(DP_n, index)
 
 def histogram_comparison(first, second, index):
     # only grey level
@@ -208,18 +213,20 @@ def edge_detection(first, second, index):
     return check_fade_or_wipe(ECR, index)
             
     
-def answer(n="news"):
-    with open(f"{n}_ground.txt", 'r') as f:
+def answer(expand = True):
+    global VIDEO
+    with open(f"{VIDEO}_ground.txt", 'r') as f:
         shot_change = []
         for l in f.readlines()[4:]:
             if l.find('~') >= 0:
-                r = l.split('~')
-                # add previous frame to correct two frame comparison
-                shot_change.append(int(r[0])-1) 
-                for rr in range(int(r[0]), int(r[1].replace('\n', ''))+1):
-                    shot_change.append(rr-1)
+                if expand:
+                    r = l.split('~')
+                    for rr in range(int(r[0]), int(r[1].replace('\n', ''))+1):
+                        shot_change.append(rr-1)
+                else:
+                    shot_change.append(l.strip('\n'))
             else:
-                shot_change.append(int(l.replace('\n', ''))-1)
+                shot_change.append(int(l.replace('\n', '')))
 
     return shot_change
 
@@ -229,31 +236,35 @@ def shot_change_detect(compare=pair_wise):
             print(f"{i:04}/{FRAME_AMOUNT}")
         if compare(f"{FILE_PREFIX}{i:04}.{FILE_EXTENSION}", f"{FILE_PREFIX}{i+1:04}.{FILE_EXTENSION}", i):
             print(i+1)
-            ANSWER.append(f"{i}+1")
+            ANSWER.append(f"{i+1}")
+            EXPAND_ANSWER.append(f"{i+1}")
 
 def draw_plot():
     global VIDEO
     global VALUE_FOR_PLOT
 
     y = VALUE_FOR_PLOT
-    answer = []
-    with open(f"{VIDEO}_ground.txt", 'r') as f:
-        for l in f.readlines()[4:]:
-            if l.find('~') >= 0:
-                r = l.split('~')
-                for rr in range(int(r[0]), int(r[1].replace('\n', ''))+1):
-                    answer.append(rr-1)
-            else:
-                answer.append(int(l.replace('\n', '')))
+    ans = answer()
 
-    an_y = [sum(y)/len(y) for a in answer]
-    new_y = [y[x] for x in answer]
+    an_y = [sum(y)/len(y) for a in ans]
+    new_y = [y[x] for x in ans]
     plt.plot(y)
-    plt.plot(answer, an_y, 'ro')
+    plt.plot(ans, an_y, 'ro')
     plt.show()
 
 def draw_PR_plot():
-    pass
+    global ANSWER
+    global EXPAND_ANSWER
+    TRUE_ANSWER = answer(False)
+    TRUE_EXPAND_ANSWER = answer()
+
+    TP = len(set(ANSWER) & set(TRUE_ANSWER))
+    FP = len(set(ANSWER) - set(TRUE_ANSWER))
+    FN = len(set(TRUE_ANSWER) - set(ANSWER))
+
+    precision = TP / (TP+FP)
+    recall = TP / (TP+FN)
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     method_group = parser.add_mutually_exclusive_group()
@@ -287,6 +298,8 @@ if __name__ == "__main__":
         FILE_PREFIX = "news_out/news-000"
         FILE_EXTENSION = "jpg"
         VIDEO = "news"
+        TWIN_COMPARISON_Tb = 42
+        TWIN_COMPARISON_Ts = 42
     elif args.soccer:
         FRAME_AMOUNT = 864
         FILE_PREFIX = "soccer_out/soccer000"
