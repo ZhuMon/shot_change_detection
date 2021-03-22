7c1385dd4531cd2b93f4b575ccbd75dcd47b186b
ZhuMon <richardlin0212@gmail.com>
2021-03-22 23:39:04 +0800

    Draw more pr-curve

--- draw_pr.py
+++ draw_pr.py
@@ -1,26 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 
 def add_line(file_name, label):
     pr = []
     with open(file_name, 'r') as f:
         lines = f.readlines()
+        # items = lines[0].strip('\n').split(' ')
+        # pr = np.array([items], dtype=float)
         for l in lines:
             items = l.strip('\n').split(' ')
+            # remove same value in precise
+            # if float(items[1]) not in pr[:,1]:
+                # new = np.array(items, dtype = float)
+                # pr = np.vstack((pr, new))
             pr.append(items)
 
     # pr = np.sort(np.array(pr, dtype=float))
-    # pr = np.array(sorted(pr, key=lambda x: x[0]), dtype=float)
-    pr = np.array(pr, dtype=float)
-    plt.plot(pr[:, 1], pr[:, 0], 'o', label=label)
+    pr = np.array(sorted(pr, key=lambda x: x[1]), dtype=float)
+    # pr = np.array(pr, dtype=float)
+    plt.plot(pr[:, 1], pr[:, 0], '-', label=label)
 
 
-add_line("pr_curve_news_gh.txt", 'news')
-add_line("pr_curve_soccer_gh.txt", 'soccer')
-# add_line("pr_curve_ngc_p.txt", 'ngc')
+# add_line("pr_curve/news_gh.txt", 'news')
+# add_line("pr_curve/soccer_gh.txt", 'soccer')
+# add_line("pr_curve/ngc_gh.txt", 'ngc')
+add_line("pr_curve/ngc_gh.txt", 'grey level histogram')
+# add_line("pr_curve/ngc_ch.txt", 'color level histogram')
+add_line("pr_curve/ngc_p.txt", 'pair wise')
+add_line("pr_curve/ngc_l.txt", 'likelihood')
+add_line("pr_curve/ngc_e.txt", 'edge detection')
 plt.xlabel("recalls")
 plt.ylabel("precise")
-plt.title("Pair wise")
+plt.title("ngc")
 plt.legend()
 plt.show()
